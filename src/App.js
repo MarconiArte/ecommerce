3cd5483ae5cfdd60f40b01df18d2dc971568b71d
Marcos Niz <marconi4305@gmail.com>
2022-08-31 17:07:58 -0300

    Se agrega navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ¡Hola mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+        <div className="header-div">
+            <span className='header-icono'>Pia Pasteleria</span>
+
+            <div className="header-secciones">
+                <p>Seccion 1</p>
+                <p>Seccion 2</p>
+                <p>Seccion 3</p>
+                <p>Seccion 4</p>
+            </div>
+        </div> 
+    </header>
   );
 }
 
